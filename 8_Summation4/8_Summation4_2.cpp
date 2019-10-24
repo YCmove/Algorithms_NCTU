3de197709c5d76503ac6422945f50788ec7d76d4
YuChingLi <y8765gd@gmail.com>
2019-10-24 18:18:13 +0800

    left one TLE!

--- 8_Summation4/8_Summation4_2.cpp
+++ 8_Summation4/8_Summation4_2.cpp
@@ -6,12 +6,20 @@ typedef long long ll;
 
 
 
-// template<class T> void print_arr(T* arr, ll n) {
-//     for (int i = 0; i < n; ++i){
-//         cout << "arr[" << i << "]:" << arr[i] << " ";
-//     }
-//     cout << endl;
-// }
+template<class T> void print_vec(T & vec){
+    for (auto it = vec.begin(); it != vec.end(); it++){
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+
+template<class T> void print_arr(T* arr, ll n) {
+    for (int i = 0; i < n; ++i){
+        cout << "arr[" << i << "]:" << arr[i] << " ";
+    }
+    cout << endl;
+}
 
 
 int main(){
@@ -19,6 +27,10 @@ int main(){
 
     while (cin >> n){
 
+        // if (n < 4){
+        //     cout << "0" << endl;
+        //     return 0;
+        // }
         ll arr[n];
 
         bool all_zero = true;
@@ -26,7 +38,7 @@ int main(){
             ll a;
             cin >> a;
 
-            if (a != 0){
+            if (a != 0 && all_zero == true){
                 all_zero = false;
             }
 
@@ -44,6 +56,8 @@ int main(){
         // cout << "----- after sort -----\n";
         // print_arr(arr, n);
 
+
+
         // invalid sequence
         if ((arr[0] >= 0) || (arr[n-1] <= 0)){
             cout << "0" << endl;
@@ -51,8 +65,45 @@ int main(){
         }
             // cout << "here !!" << endl;
 
+        // positive array and negtive array
+        // vector<ll> pos;
+        // vector<ll> neg;
+
+        // int i = 0;
+        // while (arr[i] < 0 && i < n - 1) {
+        //     neg.push_back(arr[i]);
+        //     i++;
+        // }
+
+        // int positive_idx;
+        // positive_idx = i;
+
+        // while (arr[i] >= 0 && i < n) {
+        //     pos.push_back(arr[i]);
+        //     i++;
+        // }
+
+        // print_vec(neg);
+        // print_vec(pos);
+        // cout << "positive_idx: " << positive_idx << endl;
+
+
         unordered_map<ll, pair<ll, ll> > hash_sum;
 
+        // sum of negative sequence
+        // for (int i = 0; i < n - positive_idx - 1; ++i) {
+        //     for (int j = i + 1; j < positive_idx; ++j) {
+        //         hash_sum[pos[i] + pos[j]] = {i, j}; // pair of index
+        //     }
+        // }
+
+        // sum of positive sequence
+        // for (int i = 0; i < n - positive_idx; ++i) {
+        //     for (int j = i + 1; j <= n - positive_idx; ++j) {
+        //         hash_sum[pos[i] + pos[j]] = {i, j}; // pair of index
+        //         cout << "pos[i]: " << pos[i] << ", pos[j]: " << pos[j] << endl;
+        //     }
+        // }
         // cout << "===== hash_sum =====" << endl;
         for (int i = 0; i < n-1; ++i) {
             for (int j = i + 1; j < n; ++j) {
@@ -60,7 +111,19 @@ int main(){
                 // cout << "arr[i]: " << arr[i] << ", arr[j]: " << arr[j] << endl;
             }
         }
+        // sum of negative sequence
+        // for (int i = 0; i < n - positive_idx - 1; ++i) {
+        //     for (int j = i + 1; j < positive_idx; ++j) {
+        //         cout << "neg[i]: " << neg[i] << ", neg[j]: " << neg[j] << endl;
+        //         auto h = hash_sum.find(abs(neg[i] + neg[j]));
+        //         if (h != hash_sum.end()){
+        //             cout << "1" << endl;
+        //         } else {
+        //             cout << "0" << endl;
 
+        //         };
+        //     }
+        // }
         // cout << "===== hash_sum.find =====" << endl;
         for (int i = 0; i < n - 1; ++i) {
             for (int j = i + 1; j < n; ++j) {
@@ -68,7 +131,6 @@ int main(){
                 ll sum = arr[i] + arr[j];
 
                 unordered_map<ll, pair<ll, ll> >::iterator h;
-                // h = hash_sum.find(sum);
                 if (sum == 0){
                     h = hash_sum.find(sum);
                 } else {
@@ -79,9 +141,11 @@ int main(){
                     pair<ll, ll> idx_pair = hash_sum[-sum];
                     if (idx_pair.first != i && idx_pair.first != j && idx_pair.second != i && idx_pair.second != j){
                         cout << "1" << endl;
+
                         // cout << arr[i] << ", " << arr[j] << ", " << arr[idx_pair.first] << ", " << arr[idx_pair.second] << endl; 
                         return 0;
                     }
+
                 } 
             }
         }
---
+++ 8_Summation4/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+g++ --std=c++14 8_Summation4_1.cpp -Wall -o out && ./out < data_1.txt # 1
+g++ --std=c++14 8_Summation4_1.cpp -Wall -o out && ./out < data_2.txt # 0
+g++ --std=c++14 8_Summation4_1.cpp -Wall -o out && ./out < data_3.txt # 0
+g++ --std=c++14 8_Summation4_1.cpp -Wall -o out && ./out < data_4.txt # 
