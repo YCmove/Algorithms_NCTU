7fd61d8a8a3210956a327992750fa63947318044
YuChingLi <y8765gd@gmail.com>
2019-10-24 11:05:12 +0800

    positive and negative vec

--- 8_Summation4/8_Summation4_1.cpp
+++ 8_Summation4/8_Summation4_1.cpp
@@ -5,6 +5,15 @@ using namespace std;
 typedef long long ll;
 
 
+
+template<class T> void print_vec(T & vec){
+    for (auto it = vec.begin(); it != vec.end(); it++){
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+
 template<class T> void print_arr(T* arr, ll n) {
     for (int i = 0; i < n; ++i){
         cout << "arr[" << i << "]:" << arr[i] << " ";
@@ -30,11 +39,35 @@ int main(){
             arr[i] = a;
 
         }
-        cout << "----- before sort -----\n";
-        print_arr(arr, n);
+        // cout << "----- before sort -----\n";
+        // print_arr(arr, n);
         sort(arr, arr + n);
-        cout << "----- after sort -----\n";
-        print_arr(arr, n);
+        // cout << "----- after sort -----\n";
+        // print_arr(arr, n);
+
+        // invalid sequence
+        if ((arr[0] >= 0) && (arr[n-1] <= 0)){
+            cout << "0" << endl;
+            return 0;
+        }
+
+        // positive array
+        vector<ll> pos;
+        // negtive array
+        vector<ll> neg;
+
+        for (int i = 0; i < n; ++i){
+            if (arr[i] >= 0){
+                pos.push_back(arr[i]);
+            } else {
+                neg.push_back(arr[i]);
+            }
+        }
+
+        print_vec(neg);
+        print_vec(pos);
+
+
 
 
     }
--- 8_Summation4/README.md
+++ 8_Summation4/README.md
@@ -2,6 +2,9 @@
 
 
 # Related Problem
+- [Subset sum problem](https://en.wikipedia.org/wiki/Subset_sum_problem)
+  - For example, given the set {\displaystyle \{-7,-3,-2,5,8\}}{\displaystyle \{-7,-3,-2,5,8\}}, the answer is yes because the subset {\displaystyle \{-3,-2,5\}}{\displaystyle \{-3,-2,5\}} sums to zero. The problem is NP-complete, meaning roughly that while it is easy to confirm whether a proposed solution is valid, it may inherently be prohibitively difficult to determine in the first place whether any solution exists.
+- [Find three elements in an array that sum to a zero](https://algorithms.tutorialhorizon.com/find-three-elements-in-an-array-that-sum-to-a-zero/)
 - [Number of subarrays with sum exactly = k](https://codeforces.com/blog/entry/65028)
 - [560. Subarray Sum Equals K](https://leetcode.com/problems/subarray-sum-equals-k/)
 - [907. Sum of Subarray Minimums](https://leetcode.com/problems/sum-of-subarray-minimums/)
